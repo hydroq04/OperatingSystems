a6c9ae7ca6245d1f5530938cd8f115ef412cc389
Hồ Minh Quang <82497662+hydroq04@users.noreply.github.com>
2024-10-12 21:12:48 +0700

    Update find.c

--- user/find.c
+++ user/find.c
@@ -1,46 +1,45 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
 
-int main(int argc, char *argv[]) {
-  if (argc != 1) {
-    write(1, "Usage: pingpong\n", 17);
-    exit(1);
+void find(char* path, char* fname) {
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(path, O_RDONLY)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  if (st.type != T_DIR) {
+    printf("%s is not a directory. Proceeds to exit...\n", path);
+    sleep(2);
+    printf("Usage: find [fname]\n");
+    sleep(2);
+    printf("Usage: find [dir] [fname1] [fname2] ...\n");
+    return;
   }
   else {
-    // pipe, fork, write, read, and getpid
-    int p_in[2], p_out[2];
-    pipe(p_in);
-    pipe(p_out);
-    int pid = fork();
+    
+  }
+}
 
-    if (pid == 0) {
-      close(p_in[1]);
-      char buf1[1];
-      read(p_in[0], buf1, sizeof(buf1));
-      printf("%d: received ping\n", getpid());
-      close(p_in[0]);
-
-      close(p_out[0]);
-      write(p_out[1], "o", 1);
-      close(p_out[1]);
-    }
-    else if (pid > 0) {
-      close(p_in[0]);
-      write(p_in[1], "i", 1);
-      close(p_in[1]);
-      wait((int*) 0);
-
-      close(p_out[0]);
-      char buf2[1];
-      read(p_out[1], buf2, sizeof(buf2));
-      printf("%d: received pong\n", getpid());
-      close(p_out[1]);
-    }
-    else {
-      write(1, "fork error\n", 12);
-      exit(1);
-    }
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    find(".", argv[1]);
+  }
+  else for (int i = 2; i < argc; ++i) {
+    find(argv[1], argv[i]);
   }
   exit(0);
 }
