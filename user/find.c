f498a8d6ae8f3e15971246d6131c1d88373f9b9d
Hồ Minh Quang <82497662+hydroq04@users.noreply.github.com>
2024-10-23 10:05:05 +0700

    Update find.c
    Add multiple find feature

--- user/find.c
+++ user/find.c
@@ -8,60 +8,71 @@ void find(char* root, char* name){
   char buf[512], *p;
   struct dirent de;
   struct stat st;
-    //open root
+  
+  // Open starting directory
   int fd;
-  if((fd = open(root,O_RDONLY)) < 0){
+  if ((fd = open(root,O_RDONLY)) < 0){
     printf("find: cannot open %s\n",root);
     return;
 
   }
 
-//kiem tra do sau cua direction
-    if(strlen(root) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("find: path %s too long\n", root);
-      close(fd);
-      return;
-    }
+  // Avoid long directory name
+  if (strlen(root) + 1 + DIRSIZ + 1 > sizeof buf){
+    printf("find: path %s too long\n", root);
+    close(fd);
+    return;
+  }
 
-    //them dau '/' vao sau duong dan
-    strcpy(buf, root);
-    p = buf+strlen(buf);
-    *p = '/';
+  // Add seperator '/' to sub-directory
+  strcpy(buf, root);
+  p = buf + strlen(buf);
+  *p = '/';
 
-//lay stat cua cac muc ben trong thu muc
-  while(read(fd,&de,sizeof(de)) == sizeof(de)){
-    if(de.inum == 0 || strcmp(de.name,".") == 0 || strcmp(de.name,"..") == 0){
+  // Get file/folder stats
+  while (read(fd,&de,sizeof(de)) == sizeof(de)) {
+    if (de.inum == 0 || strcmp(de.name,".") == 0 || strcmp(de.name,"..") == 0) {
       continue;
     }
+    
     char temp[512], *t;
     strcpy(temp, buf);
     t = temp + strlen(temp);
     memmove(t,de.name,DIRSIZ);
-  //stat
-  if(stat(temp,&st) < 0){
-    return;
-    continue;
-  }
-  switch(st.type){
-    case T_DEVICE:
-    case T_FILE:    
-    if(strcmp(de.name,name) == 0){
-      printf("%s\n",temp);
+    
+    // Invalid stat
+    if (stat(temp,&st) < 0) {
+      return;
+      continue;
     }
-    break;
-    case T_DIR:
-    find(temp,name);
+    
+    switch (st.type) {
+      case T_DEVICE:
+      // Print file directory
+      case T_FILE:    
+      if (strcmp(de.name,name) == 0) {
+        printf("%s\n",temp);
+      }
       break;
-    }//end switch/case
-  }//end while
-    close(fd);
+      // Recursion
+      case T_DIR:
+      find(temp,name);
+        break;
+    }
+    
+  }
+  
+  close(fd);
 }
 
 int main(int argc, char* argv[]){
-    if(argc <= 1){
-        printf("usage: find the paths to the file..\n");
-        exit(1);
-    }
-        find(argv[1],argv[2]);
-        exit(0);
+  if (argc <= 1){
+      printf("usage: find <dir> <filename 1> [filename 2] [...]\n");
+      exit(1);
+  }
+  for (int i = 2; i < argc, ++i) {
+    find(argv[1], argv[i]);
+  }
+
+  exit(0);
 }
